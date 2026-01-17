9b9bb1de51d675e037d237ac55213b49ef74dbba
shahbazpk <shabaznlp@gmail.com>
2026-01-17 23:24:50 +0500

    Time: 28 ms (100.00%) | Memory: 20.1 MB (5.19%) - LeetSync

--- 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.py
+++ 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.py
@@ -1,18 +1,29 @@
 class Solution:
     def largestSquareArea(self, bottomLeft: List[List[int]], topRight: List[List[int]]) -> int:
-        max_size = 0
+        maximal_size = 0 
 
-        for (bottom_left_i, top_right_i), (
-            bottom_left_j,
-            top_right_j,
-        ) in combinations(zip(bottomLeft, topRight), 2):
-            w = min(top_right_i[0], top_right_j[0]) - max(
-                bottom_left_i[0], bottom_left_j[0]
-            )
-            h = min(top_right_i[1], top_right_j[1]) - max(
-                bottom_left_i[1], bottom_left_j[1]
-            )
+        for i in range(len(bottomLeft)): 
+            x10, y10 = bottomLeft[i]
+            x11, y11 = topRight[i]
+            if y11-y10<=maximal_size or x11-x10<=maximal_size: 
+                continue
 
-            max_size = max(max_size, min(w, h))
+            for j in range(i+1, len(bottomLeft)): 
+                x20, y20 = bottomLeft[j]
+                x21, y21 = topRight[j]
+                if y21-y20<=maximal_size or x21-x20<=maximal_size: 
+                    continue
 
-        return max_size * max_size
+                if x21 <= x10 or y21<=y10 or x20>=x11 or y20>=y11: 
+                    continue 
+                size_x_intersect = min(x11, x21) - max(x10, x20)
+                if size_x_intersect < maximal_size: 
+                    continue 
+                size_y_intersect = min(y11, y21) - max(y10, y20)
+                if size_y_intersect < maximal_size:
+                    continue 
+                maximal_size = min(size_x_intersect, size_y_intersect)
+                
+
+
+        return maximal_size**2
