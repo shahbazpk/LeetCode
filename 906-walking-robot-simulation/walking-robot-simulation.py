449b357bc5fba7406d5fcb3b44c6e9659f1aeca4
Shahbaz Akbar <shabaznlp@gmail.com>
2026-04-06 23:52:02 +0500

    Time: 24 ms (93.04%) | Memory: 23.6 MB (99.39%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.py
+++ 906-walking-robot-simulation/walking-robot-simulation.py
@@ -1,37 +1,26 @@
 class Solution:
-    def __init__(self):
-        self.HASH_MULTIPLIER = (
-            60013
-        )
-
     def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
-        obstacle_set = {self._hash_coordinates(x, y) for x, y in obstacles}
-
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-        x, y = 0, 0
-        max_distance_squared = 0
-        current_direction = 0 
-
-        for command in commands:
-            if command == -1:
-                current_direction = (current_direction + 1) % 4
-                continue
-
-            if command == -2:
-                current_direction = (current_direction + 3) % 4
-                continue
-
-            dx, dy = directions[current_direction]
-            for _ in range(command):
-                next_x, next_y = x + dx, y + dy
-                if self._hash_coordinates(next_x, next_y) in obstacle_set:
-                    break
-                x, y = next_x, next_y
-
-            max_distance_squared = max(max_distance_squared, x * x + y * y)
-
-        return max_distance_squared
-
-    def _hash_coordinates(self, x: int, y: int) -> int:
-        return x + self.HASH_MULTIPLIER * y
+        obSet=set()
+        for (x, y) in obstacles:
+            obSet.add((x, y))
+        dir=[(0, 1), (-1, 0), (0, -1), (1, 0)]
+        x, y,dx, dy, face, maxD2=0, 0, 0,1, 0, 0
+        for  c in commands:
+            if c==-2:
+                face=(face+1)&3
+                dx=dir[face][0]
+                dy=dir[face][1]
+            elif c==-1:
+                face=(face+3)&3
+                dx=dir[face][0]
+                dy=dir[face][1]
+            else:
+                for i in range(c):
+                    x+=dx
+                    y+=dy
+                    if (x, y) in obSet:
+                        x-=dx
+                        y-=dy
+                        break
+                maxD2=max(maxD2, x*x+y*y)
+        return maxD2
