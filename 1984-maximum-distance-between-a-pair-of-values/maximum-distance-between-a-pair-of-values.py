6426c859dee529727d102db3e084ba17c5b2430f
Shahbaz Akbar <shabaznlp@gmail.com>
2026-04-19 20:49:56 +0500

    Time: 33 ms (98.63%) | Memory: 36.1 MB (21.31%) - LeetSync

---
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        i, j = 0, 1
+        while i < len(nums1) and j < len(nums2):
+            i += nums1[i] > nums2[j]
+            j += 1
+        return j - i - 1
