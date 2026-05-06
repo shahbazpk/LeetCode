8d9764c585a73c5fbaff517f744c454b52e2a1d6
Shahbaz Akbar <shabaznlp@gmail.com>
2026-05-06 22:26:02 +0500

    Time: 1089 ms (5.07%) | Memory: 56.4 MB (64.50%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.py
@@ -0,0 +1,34 @@
+class Solution:
+    def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
+        m = len(boxGrid)
+        n = len(boxGrid[0])
+        result = [["" for _ in range(m)] for _ in range(n)]
+
+        for i in range(n):
+            for j in range(m):
+                result[i][j] = boxGrid[j][i]
+
+        for i in range(n):
+            result[i].reverse()
+
+        for j in range(m):
+            for i in range(n - 1, -1, -1):
+                if (
+                    result[i][j] == "."
+                ):
+                    next_row_with_stone = -1
+
+                    for k in range(i - 1, -1, -1):
+                        if result[k][j] == "*":
+                            break
+                        if (
+                            result[k][j] == "#"
+                        ):
+                            next_row_with_stone = k
+                            break
+
+                    if next_row_with_stone != -1:
+                        result[next_row_with_stone][j] = "."
+                        result[i][j] = "#"
+
+        return result      
