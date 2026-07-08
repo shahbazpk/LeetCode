13f6b80638813eff0e931e0a1a006f127424d3a8
Shahbaz Akbar <shabaznlp@gmail.com>
2026-07-08 08:29:09 +0500

    Time: 81 ms (20.09%) | Memory: 19.3 MB (85.91%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,3 +1,3 @@
 class Solution:
     def checkInclusion(self, s: str, t: str) -> bool:
-        return (c:=Counter(s))==(z:=Counter(t[:len(s)])) or any(z.update({p:-1,q:1}) or c==z for p,q in zip(t,t[len(s):]) if p!=q)
+        return (c:=Counter(s))==(z:=Counter(t[:len(s)])) or any(z.update(q) or z.subtract(p) or c==z for p,q in zip(t,t[len(s):]))
