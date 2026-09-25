c5a13917dbba9594c9bc371c97f22832ce1f563e
Shahbaz Akbar <shabaznlp@gmail.com>
2026-09-25 23:31:35 +0500

    Time: 5 ms (20.22%) | Memory: 19.7 MB (29.51%) - LeetSync

--- 1188-brace-expansion-ii/brace-expansion-ii.py
+++ 1188-brace-expansion-ii/brace-expansion-ii.py
@@ -1,26 +1,54 @@
 class Solution:
-    def braceExpansionII(self, expression: str) -> list[str]:
-        ans = set()
+    def braceExpansionII(self, expression: str) -> List[str]:
+        idx = 0
+        n = len(expression)
 
-        def dfs(s):
-            r = s.find('}')
+        def is_letter(c: str) -> bool:
+            return "a" <= c <= "z"
 
-            # No braces left
-            if r == -1:
-                ans.add(s)
-                return
+        # Recursive descent parser
+        def expr() -> set:
+            nonlocal idx
+            ret = set()
+            while True:
+                # Take the union with the result of term()
+                ret |= term()
+                # Continue if a comma is matched; otherwise, stop matching
+                if idx < n and expression[idx] == ",":
+                    idx += 1
+                    continue
+                else:
+                    break
+            return ret
 
-            # Find matching '{'
-            l = s.rfind('{', 0, r)
+        # term -> item | item term
+        def term() -> set:
+            nonlocal idx
+            # Initialize an empty set and take its Cartesian product with subsequent results
+            ret = {""}
+            # An item starts with { or a lowercase letter; continue matching only when this condition is met
+            while idx < n and (
+                expression[idx] == "{" or is_letter(expression[idx])
+            ):
+                sub = item()
+                tmp = set()
+                for left in ret:
+                    for right in sub:
+                        tmp.add(left + right)
+                ret = tmp
+            return ret
 
-            left = s[:l]
-            right = s[r + 1:]
+        # item -> letter | { expr }
+        def item() -> set:
+            nonlocal idx
+            ret = set()
+            if expression[idx] == "{":
+                idx += 1
+                ret = expr()
+            else:
+                ret = {expression[idx]}
+            idx += 1
+            return ret
 
-            # Content inside { }
-            inside = s[l + 1:r]
-
-            for part in inside.split(','):
-                dfs(left + part + right)
-
-        dfs(expression)
-        return sorted(ans)
+        ret = expr()
+        return sorted(list(ret))
